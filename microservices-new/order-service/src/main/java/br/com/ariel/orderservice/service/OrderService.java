49d5d183ca495e3b121fe9e9f110fbe2069e7bd1
Ariel Guareschi <arielguareschi@gmail.com>
2023-10-21 09:35:12 -0300

    interprocess comunnication

--- microservices-new/order-service/src/main/java/br/com/ariel/orderservice/service/OrderService.java
+++ microservices-new/order-service/src/main/java/br/com/ariel/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,8 +34,8 @@ public class OrderService {
 
 
         // chama o Intentory Service, a grava uma ordem se o produto esta em estoque
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
